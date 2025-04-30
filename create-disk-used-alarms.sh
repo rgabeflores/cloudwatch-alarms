a13a445ea22c449e31882d4290645294461441e0
Gabe Flores <24464548+rgabeflores@users.noreply.github.com>
2025-04-30 11:43:28 -0700

    adjust threshold for disk used

--- create-disk-used-alarms.sh
+++ create-disk-used-alarms.sh
@@ -5,6 +5,6 @@ action=$2
 
 while IFS= read -r instanceid; do
 	echo "Creating disk used percent alarm for instance: $instanceid" 
-	aws cloudwatch put-metric-alarm --alarm-name disk-used-$instanceid --alarm-description "Alarm when $instanceid disk used percentage is above 85%" --metric-name disk_used_percent --namespace runotp-prod-cwagent --statistic Average --period 60 --threshold 85 --comparison-operator GreaterThanThreshold --dimensions "Name=InstanceId,Value=$instanceid" --evaluation-periods 2 --alarm-actions $action --unit Percent --tags Key=TargetGroup,Value=$instanceid Key=Description,Value="SOC2 Compliance" --profile $MFA_PROFILE
+	aws cloudwatch put-metric-alarm --alarm-name disk-used-$instanceid --alarm-description "Alarm when $instanceid disk used percentage is above 85%" --metric-name disk_used_percent --namespace runotp-prod-cwagent --statistic Average --period 60 --threshold 70 --comparison-operator GreaterThanThreshold --dimensions "Name=InstanceId,Value=$instanceid" --evaluation-periods 2 --alarm-actions $action --unit Percent --tags Key=Instance,Value=$instanceid Key=Description,Value="SOC2 Compliance" --profile $MFA_PROFILE
 done < $file
 
